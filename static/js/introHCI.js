e85ded66b2f1715c621f2d2b16446a705463d742
yuerongX <y3xiao@ucsd.edu>
2021-01-14 00:05:29 -0800

    added Javascript!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -5,5 +5,11 @@ $(document).ready(function() {
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
+            $("a.thumbnail").click(projectClick);
             });
+}
+
+function projectClick(e) {
+    e.preventDefault();   
+    $(this).css("background-color", "#7fff00");
 }
